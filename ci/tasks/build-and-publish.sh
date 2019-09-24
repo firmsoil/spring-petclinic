64ec5e9ffe38c8f6aa5a875f296304be27ac2182
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-09-23 22:11:25 -0400

    Updates ci scripts

--- ci/tasks/build-and-publish.sh
+++ ci/tasks/build-and-publish.sh
@@ -7,14 +7,7 @@ echo "Build version: ${version}"
 
 ./ci-scripts/ci/tasks/create-maven-settings-xml.sh
 
-export ROOT_FOLDER=$( pwd )
-export BUILD_ID=`cat ${ROOT_FOLDER}/meta/build-id`
-export BUILD_NAME=`cat ${ROOT_FOLDER}/meta/build-name`
-export BUILD_TEAM_NAME=`cat ${ROOT_FOLDER}/meta/build-team-name`
-export BUILD_PIPELINE_NAME=`cat ${ROOT_FOLDER}/meta/build-pipeline-name`
-export BUILD_JOB_NAME=`cat ${ROOT_FOLDER}/meta/build-job-name`
-export ATC_EXTERNAL_URL=`cat ${ROOT_FOLDER}/meta/atc-external-url`
-export BUILD_URI=${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_PIPELINE_NAME}/build/${BUILD_NAME}
+repository=$(pwd)/distribution-repository
 
 cd code-repo
 
@@ -25,7 +18,8 @@ echo "Running mvn deploy command"
     -s ${HOME}/.m2/settings.xml
 ./mvnw deploy \
     -DskipTests \
-    -s ${HOME}/.m2/settings.xml
+    -s ${HOME}/.m2/settings.xml \
+    -DaltDeploymentRepository=distribution::default::file://${repository}
 
 # Create file with tag name to be used in later put step
 echo "version-${version}-artifactory-deploy" > ../results/tag.txt
